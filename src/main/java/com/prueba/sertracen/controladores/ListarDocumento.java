696c906996c3e943db76b740cbdab1f2e93bc774
Raúl Granados <raul.granados12@gmail.com>
2022-07-06 15:28:24 -0600

    Agregando metodo para listar documentos

--- src/main/java/com/prueba/sertracen/controladores/ListarDocumento.java
+++ src/main/java/com/prueba/sertracen/controladores/ListarDocumento.java
@@ -1,4 +1,37 @@
 package com.prueba.sertracen.controladores;
 
+import com.prueba.sertracen.modelos.Documento;
+import com.prueba.sertracen.servicios.DocumentoDao;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Inject;
+
+import java.util.List;
+
 public class ListarDocumento {
+
+    private List<Documento> documentos;
+
+    @Inject
+    private DocumentoDao documentoDao;
+
+    @PostConstruct
+    public void init(){
+        this.documentos = documentoDao.documentos();
+    }
+
+    public List<Documento> getDocumentos() {
+        return documentos;
+    }
+
+    public void setDocumentos(List<Documento> documentos) {
+        this.documentos = documentos;
+    }
+
+    public DocumentoDao getDocumentoDao() {
+        return documentoDao;
+    }
+
+    public void setDocumentoDao(DocumentoDao documentoDao) {
+        this.documentoDao = documentoDao;
+    }
 }
